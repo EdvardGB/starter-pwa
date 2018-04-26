76ab4659297d96831b2615b0e146491d60d58e09
EdvardGB <edvard_bakken@live.no>
2018-04-26 09:52:10 +0200

    importing of react & react-dom fails

--- src/main/resources/assets/js/react.js
+++ src/main/resources/assets/js/react.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+var ReactDOM = require('react-dom');
+var React = require('react');
 
 class Enonic extends React.Component {
   render() {
